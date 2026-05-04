d41770b76752d2f78382e0fd51a18e3994dc1517
harrysllaluw <57797087+harrysllaluw@users.noreply.github.com>
2026-05-05 04:01:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 import google.generativeai as genai
 
-# Ambil Variabel
+# Ambil Variabel dari Railway
 TOKEN = os.getenv('BOT_TOKEN')
 KUNCI = os.getenv('GEMINI_KEY')
 
 # Setel Gemini
 genai.configure(api_key=KUNCI)
-model = genai.GenerativeModel('gemini-pro')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -19,15 +18,16 @@ dp = Dispatcher()
 async def chat(message: Message):
     if not message.text: return
     try:
+        model = genai.GenerativeModel('gemini-1.5-flash')
         respon = model.generate_content(message.text)
         await message.answer(respon.text)
     except Exception as e:
         print(f"Error: {e}")
-        await message.answer("Bolu lagi pening, Bos. Cek kuncinya!")
+        await message.answer("Bolu lagi pening, Bos. Cek koneksi!")
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    print(">>> BOLU SIAP! <<<")
+    print(">>> BOLU HIDUP! <<<")
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
